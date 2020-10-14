b90581ea619dc86e5fc0bc976e405d13e887aa91
ArianaAnd <andrisan_ariana@yahoo.com>
2020-10-14 20:53:51 +0300

    Curs Testare
    14.10

---
+++ Demo/Curs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Curs
+    {
+        private int _cursId { get; set;}
+        private string _Nume{ get; set;}
+        private int _durata{ get; set;}
+
+        public Curs(int cursId, string Nume, int durata)
+        {
+            _cursId = cursId;
+            _Nume = Nume;
+            _durata = durata;
+        }
+        //~Curs()
+        //{
+        //    Console.WriteLine("I am a destructor");
+        //}
+
+        //public void SetareCurs(int idCurs, string numeCurs, int durataCurs)
+        //{
+        //    cursId = idCurs;
+        //    Nume = numeCurs;
+        //    Durata = durataCurs;
+
+
+        //}
+
+        //public void SetCursId(int cursId)
+        //{
+        //    _cursId = cursId;
+        //}
+        //public int GetCursId()
+        //{
+        //    return _cursId;
+        //}
+
+        //public string NumeCurs()
+        //{
+        //    return Nume; 
+        //}
+    }
---
+++ Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curs myCurs = new Curs();
+            //myCurs.SetareCurs(1, "Testare Automatizata", 12);
+            //Console.WriteLine(myCurs.NumeCurs());
+            myCurs
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
