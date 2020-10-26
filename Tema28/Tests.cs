1c6362d243aa3013138142b54cbaa8431e0b0818
ArianaAnd <andrisan_ariana@yahoo.com>
2020-10-26 13:06:55 +0200

    Selenium Project

--- Tema28/Tests.cs
+++ Tema28/Tests.cs
@@ -22,158 +22,158 @@ namespace Tema28
     public class Tests : Hooks
     {
         [Test]
-        //public void GoogleImage()
-        //{
-        //    Driver.Manage().Window.Maximize();
+        public void GoogleImage()
+        {
+            Driver.Manage().Window.Maximize();
 
 
-        //    Driver.Navigate().GoToUrl("http://google.com/");
-        //    // Arrange
-        //    Google homePage = new Google(Driver);
+            Driver.Navigate().GoToUrl("http://google.com/");
+            // Arrange
+            Google homePage = new Google(Driver);
 
-        //    // Act
-        //    Driver.SwitchTo().Frame(0);
-        //    homePage.AcceptGoogleTerms();
-        //    homePage.SearchGoogleImage("paris");
-        //    homePage.SelectGoogleImage();
+            // Act
+            Driver.SwitchTo().Frame(0);
+            homePage.AcceptGoogleTerms();
+            homePage.SearchGoogleImage("paris");
+            homePage.SelectGoogleImage();
 
-        //    Driver.Navigate().Back();
+            Driver.Navigate().Back();
 
-        //    //Assert
-        //    NUnit.Framework.Assert.IsTrue(!homePage.imageFrame.Displayed);
+            //Assert
+            NUnit.Framework.Assert.IsTrue(!homePage.imageFrame.Displayed);
 
-        //}
-
-
-        //public void DemoqaForm()
-        //{
-        //    Driver.Manage().Window.Maximize();
+        }
 
 
-        //    Driver.Navigate().GoToUrl("https://demoqa.com/automation-practice-form");
-        //    // Arrange
-        //    Demoqa formPage = new Demoqa(Driver);
+        public void DemoqaForm()
+        {
+            Driver.Manage().Window.Maximize();
 
 
-        //    //Act
-        //    formPage.FillDemoqaForm();
-
-        //    //Assert
-
-        //    NUnit.Framework.Assert.IsTrue(formPage.VerifyElementDisplayed(formPage.submitButton));
-        //}    
+            Driver.Navigate().GoToUrl("https://demoqa.com/automation-practice-form");
+            // Arrange
+            Demoqa formPage = new Demoqa(Driver);
 
 
-        //public void DemoqaTextBoxTest()
-        //{
-        //    Driver.Manage().Window.Maximize();
-        //    Driver.Navigate().GoToUrl("https://demoqa.com/text-box");
+            //Act
+            formPage.FillDemoqaForm();
 
-        //    //Arrange
-        //    DemoqaTextBox textboxPage = new DemoqaTextBox(Driver);
+            //Assert
 
-        //    //Act
-        //    textboxPage.FillDemoqaTextBox();
+            NUnit.Framework.Assert.IsTrue(formPage.VerifyElementDisplayed(formPage.submitButton));
+        }
 
-        //    //Assert
-        //   NUnit.Framework.Assert.IsTrue(textboxPage.VerifyElementDisplayed(textboxPage.submit));
-        //}
 
-        //public void TeatruSicaTest()
-        //{
-        //    Driver.Manage().Window.Maximize();
-        //    Driver.Navigate().GoToUrl("https://www.teatrulsicaalexandrescu.ro/?lang=en");
+        public void DemoqaTextBoxTest()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("https://demoqa.com/text-box");
 
-        //    //Arrange
-        //    TeatruSica testPage = new TeatruSica(Driver);
+            //Arrange
+            DemoqaTextBox textboxPage = new DemoqaTextBox(Driver);
 
-        //    //Act
-        //    testPage.GoToTeatruSicaPage();
+            //Act
+            textboxPage.FillDemoqaTextBox();
 
-        //    //Assert
-        //    NUnit.Framework.Assert.IsTrue(testPage.NameShowReturn.Displayed);
-        //}
+            //Assert
+            NUnit.Framework.Assert.IsTrue(textboxPage.VerifyElementDisplayed(textboxPage.submit));
+        }
 
-        //public void Untold()
-        //{
-        //    Driver.Manage().Window.Maximize();
-        //    Driver.Navigate().GoToUrl("https://untold.com/");
+        public void TeatruSicaTest()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("https://www.teatrulsicaalexandrescu.ro/?lang=en");
 
-        //    //Arrange
-        //    Untold untoldTest = new Untold(Driver);
+            //Arrange
+            TeatruSica testPage = new TeatruSica(Driver);
 
-        //    //Act
-        //    untoldTest.GoToUntoldPage();
+            //Act
+            testPage.GoToTeatruSicaPage();
 
-        //    //Assert
-        //    NUnit.Framework.Assert.IsTrue(untoldTest.logo.Displayed);
-        //}
+            //Assert
+            NUnit.Framework.Assert.IsTrue(testPage.NameShowReturn.Displayed);
+        }
 
-        //public void LoginIntoApplication()
-        //{
-        //    Driver.Manage().Window.Maximize();
-        //    Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
-        //    //Arrange
-        //    LoginPage myLoginPageuser = new LoginPage(Driver);
-        //    HomePage myHomePage = new HomePage(Driver);
-        //    //Act
-        //    myLoginPageuser.LoginIntoApplication("admin.test3@gmail.com", "password123");
-        //    //Assert
-        //    NUnit.Framework.Assert.IsTrue(myHomePage.DeconectareButton.Displayed);
-        //}
-        //[Test]
-        //public void LoginIntoApplicationWithInvalidEmailAndPassword()
-        //{
-        //    Driver.Manage().Window.Maximize();
-        //    Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
-        //    //Arrage
-        //    HomePage myHomePage = new HomePage(Driver);
-        //    myHomePage.GoToAutentificarePage();
-        //    LoginPage myLoginPageadmin = new LoginPage(Driver);
-        //    //Act
-        //    myLoginPageadmin.LoginIntoApplication("abcedfs@gmail.com", "dasdcsdfds");
-        //    //Asert
-        //    NUnit.Framework.Assert.IsTrue(myLoginPageadmin.VerifyElementDisplayed(myHomePage.DeconectareButton));
-        //}
-        //public void AdaugaInCosUtilizator()
-        //{
-        //    Driver.Manage().Window.Maximize();
-        //    Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
-            
-        //    //Arrange
-        //    LoginPage myLoginPage = new LoginPage(Driver);
-        //    ProductPage myProductPage = new ProductPage(Driver);
-        //    HomePage myHomePage = new HomePage(Driver);
-        //    //Act
-        //    myProductPage.AdaugaInCos();
-        //    //Assert
-        //    NUnit.Framework.Assert.IsTrue(myHomePage.DeconectareButton.Displayed);
-        //}
+        public void Untold()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("https://untold.com/");
 
-        //public void Inscriere()
-        //{
-        //    Driver.Manage().Window.Maximize();
-        //    Driver.Navigate().GoToUrl("http://demosite.casqad.org/auth/register");
-        //    //Arrange
-        //    InregistrationPage inscriere = new InregistrationPage(Driver);
-        //    HomePage myhomepage = new HomePage(Driver);
-        //    //Act
-        //    inscriere.GoToInregistrationPage();
-        //    myhomepage.GoToAutentificarePage();
-        //    //Assert
-        //    NUnit.Framework.Assert.IsTrue(myhomepage.DeconectareButton.Displayed);
-        //}
+            //Arrange
+            Untold untoldTest = new Untold(Driver);
 
-        //public void ClickNavbar()
-        //{
-        //    Driver.Manage().Window.Maximize();
-        //    Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
-        
-        //    Navbar myNavbar = new Navbar(Driver);
-        //    myNavbar.Navigare();
-        //     NUnit.Framework.Assert.IsTrue(myNavbar.laptopuri.Displayed);
-            
-        //}
+            //Act
+            untoldTest.GoToUntoldPage();
+
+            //Assert
+            NUnit.Framework.Assert.IsTrue(untoldTest.logo.Displayed);
+        }
+
+        public void LoginIntoApplication()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
+            //Arrange
+            LoginPage myLoginPageuser = new LoginPage(Driver);
+            HomePage myHomePage = new HomePage(Driver);
+            //Act
+            myLoginPageuser.LoginIntoApplication("admin.test3@gmail.com", "password123");
+            //Assert
+            NUnit.Framework.Assert.IsTrue(myHomePage.DeconectareButton.Displayed);
+        }
+        [Test]
+        public void LoginIntoApplicationWithInvalidEmailAndPassword()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
+            //Arrage
+            HomePage myHomePage = new HomePage(Driver);
+            myHomePage.GoToAutentificarePage();
+            LoginPage myLoginPageadmin = new LoginPage(Driver);
+            //Act
+            myLoginPageadmin.LoginIntoApplication("abcedfs@gmail.com", "dasdcsdfds");
+            //Asert
+            NUnit.Framework.Assert.IsTrue(myLoginPageadmin.VerifyElementDisplayed(myHomePage.DeconectareButton));
+        }
+        public void AdaugaInCosUtilizator()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
+
+            //Arrange
+            LoginPage myLoginPage = new LoginPage(Driver);
+            ProductPage myProductPage = new ProductPage(Driver);
+            HomePage myHomePage = new HomePage(Driver);
+            //Act
+            myProductPage.AdaugaInCos();
+            //Assert
+            NUnit.Framework.Assert.IsTrue(myHomePage.DeconectareButton.Displayed);
+        }
+
+        public void Inscriere()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("http://demosite.casqad.org/auth/register");
+            //Arrange
+            InregistrationPage inscriere = new InregistrationPage(Driver);
+            HomePage myhomepage = new HomePage(Driver);
+            //Act
+            inscriere.GoToInregistrationPage();
+            myhomepage.GoToAutentificarePage();
+            //Assert
+            NUnit.Framework.Assert.IsTrue(myhomepage.DeconectareButton.Displayed);
+        }
+
+        public void ClickNavbar()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
+
+            Navbar myNavbar = new Navbar(Driver);
+            myNavbar.Navigare();
+            NUnit.Framework.Assert.IsTrue(myNavbar.laptopuri.Displayed);
+
+        }
 
         public void ClickAdmin()
         {
